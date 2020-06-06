cd202be2ee8fe30f72cd0722e5c1d3f3d82e38fb
Ahmed Radhouane Belkilani <arbelkilani@gmail.com>
2020-06-06 03:05:07 +0100

    - adding DiscoverFragment.kt and watchlistFragment - set the bottom navigation view

--- app/src/main/java/com/arbelkilani/bingetv/presentation/view/DashboardActivity.kt
+++ app/src/main/java/com/arbelkilani/bingetv/presentation/view/DashboardActivity.kt
@@ -1,22 +1,60 @@
 package com.arbelkilani.bingetv.presentation.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.arbelkilani.bingetv.R
 import com.arbelkilani.bingetv.data.model.base.ApiResponse
 import com.arbelkilani.bingetv.data.model.tv.Tv
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_dashboard.*
 
-class DashboardActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity(),
+    BottomNavigationView.OnNavigationItemSelectedListener {
 
     private val TAG = SplashActivity::class.java.simpleName
+
+    private lateinit var airingTodayData : ApiResponse<Tv>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
-        val data = intent.getParcelableExtra<ApiResponse<Tv>>("DATA")
-        Log.i(TAG, "data = $data")
+        airingTodayData = intent.getParcelableExtra("DATA")!!
+
+        if (savedInstanceState == null) {
+            loadFragment(DiscoverFragment.newInstance(airingTodayData))
+        }
+
+        initViews()
+
+    }
+
+    private fun initViews() {
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, fragment, fragment::class.java.simpleName)
+            .commit()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_discover -> {
+                loadFragment(DiscoverFragment())
+                return true
+            }
+
+            R.id.menu_watch_list -> {
+                loadFragment(WatchlistFragment())
+                return true
+            }
+        }
+
+        return false
     }
 }
