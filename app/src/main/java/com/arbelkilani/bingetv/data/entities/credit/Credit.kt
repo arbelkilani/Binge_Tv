5f0e5d3399b0f0011eb0637c4080360947d635f8
Ahmed Radhouane Belkilani <arbelkilani@gmail.com>
2020-07-13 00:45:30 +0100

    - refactor credit poster

--- app/src/main/java/com/arbelkilani/bingetv/data/entities/credit/Credit.kt
+++ app/src/main/java/com/arbelkilani/bingetv/data/entities/credit/Credit.kt
@@ -7,14 +7,18 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class Credit(
     val character: String,
-    @SerializedName("credit_id") val creditId: String,
-    val gender: Int?,
-    val id: Int,
-    val name: String,
-    val order: Int,
-    @SerializedName("profile_path") val _profilePath: String?
-) : Parcelable {
+    @SerializedName("credit_id")
+    val creditId: String,
 
+    val gender: Int?,
+
+    val id: Int,
+
+    val name: String,
+
+    val order: Int,
+
+    @SerializedName("profile_path")
     val profilePath: String?
-        get() = "https://image.tmdb.org/t/p/w185${this._profilePath}"
-}
+
+) : Parcelable
